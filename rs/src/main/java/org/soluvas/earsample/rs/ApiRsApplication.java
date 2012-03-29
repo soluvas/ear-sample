90ffd0846346245e43884b399f57a3c7d9b8b31a
Hendy Irawan <hendy@soluvas.com>
2012-03-30 01:08:53 +0700

    Even using getSingletons() doesn't work :(

--- rs/src/main/java/org/soluvas/earsample/rs/ApiRsApplication.java
+++ rs/src/main/java/org/soluvas/earsample/rs/ApiRsApplication.java
@@ -3,8 +3,14 @@
  */
 package org.soluvas.earsample.rs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig.Feature;
 
 /**
  * @author ceefour
@@ -14,4 +20,14 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/")
 public class ApiRsApplication extends Application {
 
+	@Override
+	public Set<Object> getSingletons() {
+		Set<Object> singletons = new HashSet<Object>(super.getSingletons());
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(Feature.INDENT_OUTPUT, true);
+		objectMapper.configure(Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+		objectMapper.configure(Feature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+		singletons.add(objectMapper);
+		return singletons;
+	}
 }
