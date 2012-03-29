38599c437112f73f5de270de12887b9eaf3af438
Hendy Irawan <hendy@soluvas.com>
2012-03-30 01:10:51 +0700

    The custom configured ObjectMapper works, but JBoss's RESTeasy won't use it!

--- rs/src/main/java/org/soluvas/earsample/rs/JacksonContextResolver.java
+++ rs/src/main/java/org/soluvas/earsample/rs/JacksonContextResolver.java
@@ -1,14 +1,20 @@
 package org.soluvas.earsample.rs;
 
+import java.io.IOException;
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig.Feature;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.soluvas.earsample.data.Article;
 
 @Provider @Produces(MediaType.APPLICATION_JSON)
 public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
@@ -16,12 +22,18 @@ public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
 	private transient Logger log = LoggerFactory.getLogger(JacksonContextResolver.class);
 	private ObjectMapper objectMapper;
 
-	public JacksonContextResolver() {
+	public JacksonContextResolver() throws JsonGenerationException, JsonMappingException, IOException {
 		log.info("Using my own Jackson ObjectMapper");
 		objectMapper = new ObjectMapper();
 		objectMapper.configure(Feature.INDENT_OUTPUT, true);
 		objectMapper.configure(Feature.WRITE_DATES_AS_TIMESTAMPS, false);
 		objectMapper.configure(Feature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+		
+		log.info("Test date: {}", objectMapper.writeValueAsString(new DateTime()));
+		log.info("Test pretty output: {}", objectMapper
+				.writeValueAsString(new Article("Launching AksiMata",
+						"AksiMata : Ada apa di sana?",
+						"Webapp yang keren dan oke punya.", "Hendy Irawan")));
 	}
 	
 	@Override
