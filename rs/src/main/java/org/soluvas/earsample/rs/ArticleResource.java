61d4ecbbbf6c03b7c0acac7d344120e1f80787fb
Hendy Irawan <hendy@soluvas.com>
2012-03-30 03:52:17 +0700

    Trying Solder. Doesn't work. See discussion: https://community.jboss.org/thread/178845

--- rs/src/main/java/org/soluvas/earsample/rs/ArticleResource.java
+++ rs/src/main/java/org/soluvas/earsample/rs/ArticleResource.java
@@ -2,17 +2,23 @@ package org.soluvas.earsample.rs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.solder.resourceLoader.Resource;
 import org.soluvas.earsample.data.Article;
 
 @Path("article")
 public class ArticleResource {
 
+	@Inject @Resource("ear-sample.properties") Properties props;
+	
 	@GET @Produces(MediaType.APPLICATION_JSON)
 	public List<Article> getIndex() {
 		ArrayList<Article> articles = new ArrayList<Article>();
@@ -22,4 +28,11 @@ public class ArticleResource {
 				"Makin bagus dan selalu mantab gan.", "Cinta Sejati"));
 		return articles;
 	}
+	
+	@GET @Produces(MediaType.APPLICATION_JSON)
+	@Path("props")
+	public Properties getProps() {
+		return props;
+	}
+	
 }
