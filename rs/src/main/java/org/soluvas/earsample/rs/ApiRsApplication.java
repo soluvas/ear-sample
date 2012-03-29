71efaed4dcbff8c6f2cfcf0d44bf826d48f1ad4b
Hendy Irawan <hendy@soluvas.com>
2012-03-30 01:09:15 +0700

    Remove the non-portable way.

--- rs/src/main/java/org/soluvas/earsample/rs/ApiRsApplication.java
+++ rs/src/main/java/org/soluvas/earsample/rs/ApiRsApplication.java
@@ -3,14 +3,8 @@
  */
 package org.soluvas.earsample.rs;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.SerializationConfig.Feature;
 
 /**
  * @author ceefour
@@ -20,14 +14,14 @@ import org.codehaus.jackson.map.SerializationConfig.Feature;
 @ApplicationPath("/")
 public class ApiRsApplication extends Application {
 
-	@Override
-	public Set<Object> getSingletons() {
-		Set<Object> singletons = new HashSet<Object>(super.getSingletons());
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.configure(Feature.INDENT_OUTPUT, true);
-		objectMapper.configure(Feature.WRITE_DATES_AS_TIMESTAMPS, false);
-		objectMapper.configure(Feature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
-		singletons.add(objectMapper);
-		return singletons;
-	}
+//	@Override
+//	public Set<Object> getSingletons() {
+//		Set<Object> singletons = new HashSet<Object>(super.getSingletons());
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		objectMapper.configure(Feature.INDENT_OUTPUT, true);
+//		objectMapper.configure(Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+//		objectMapper.configure(Feature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+//		singletons.add(objectMapper);
+//		return singletons;
+//	}
 }
